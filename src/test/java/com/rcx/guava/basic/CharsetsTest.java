0c47c7d16ea698d7f60c7808b9fa414386e12293
renchunxiao <rcx123tiancai@126.com>
2015-03-17 11:52:44 +0800

    add Strings 和  Charsets 单元测试

--- src/test/java/com/rcx/guava/basic/CharsetsTest.java
+++ src/test/java/com/rcx/guava/basic/CharsetsTest.java
@@ -1,15 +1,27 @@
 package com.rcx.guava.basic;
 
+import java.nio.charset.StandardCharsets;
+
 import org.junit.Test;
 
 import com.google.common.base.Charsets;
 
-public class CharsetsTest
-{
+/**
+ * Charsets 是一个常量工厂，给出了 6 个 Java 承诺了全平台支持的字符集
+ * 
+ * @author chunxiao.ren created on 2015年3月17日 上午11:33:43 
+ * @version 1.0
+ */
+public class CharsetsTest {
+
+    /**
+     * 在 JDK 7 当中有个 StandardCharsets 类与 Charsets 的作用是一样的可以使用
+     */
     @Test
-    public void method()
-    {
+    public void test() {
         byte[] cs = "string".getBytes(Charsets.UTF_8);
+        byte[] cs1 = "string".getBytes(StandardCharsets.UTF_8);
         System.out.println(cs[0]);
+        System.out.println(cs1[0]);
     }
 }
--- src/test/java/com/rcx/guava/basic/StringsTest.java
+++ src/test/java/com/rcx/guava/basic/StringsTest.java
@@ -1,16 +1,28 @@
 package com.rcx.guava.basic;
 
+import static junit.framework.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.google.common.base.Strings;
 
-import static junit.framework.Assert.assertEquals;
+/**
+ * 操作 String 或者 CharSequence 的静态方法工具类
+ * 
+ * @author chunxiao.ren created on 2015年3月17日 上午11:35:31 
+ * @version 1.0
+ */
+public class StringsTest {
 
-public class StringsTest
-{
+    /**
+     * 在字符串后面填充字符
+     * Strings.padEnd(String string, int minLength, char padChar) 需要 3 个参数
+     * string 为需要填充的字符串
+     * minLength 为填充后的长度，如果长度小于 string 的长度，则直接返回 string
+     * padChar 填充到后面的字符
+     */
     @Test
-    public void padEndTest()
-    {
+    public void padEndTest() {
         String original = "foo";
         String expected = "fooxxx";
         String returnString = Strings.padEnd(original, 6, 'x');
@@ -20,9 +32,12 @@ public class StringsTest
         assertEquals(original, returnString1);
     }
 
+    /**
+     * 在字符串前面填充字符
+     * 参数与 padEnd 方法类似
+     */
     @Test
-    public void padStartTest()
-    {
+    public void padStartTest() {
         String original = "foo";
         String expected = "xxxfoo";
         String returnString = Strings.padStart(original, 6, 'x');
@@ -32,9 +47,36 @@ public class StringsTest
         assertEquals(original, returnString1);
     }
 
+    /**
+     * 将指定的字符串重复 n 次
+     */
     @Test
-    public void empetyTest()
-    {
+    public void repeatTest() {
+        String original = "foo";
+        String expected = "foofoofoo";
+        String actual = Strings.repeat(original, 3);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * 查找两个字符串相同的前缀或相同的后缀
+     */
+    @Test
+    public void commonPrefixAndcommonSuffixTest() {
+        String s1 = "asdfg";
+        String s2 = "asdde";
+        String s3 = "fgasd";
+        String s4 = "deasd";
+        String expected = "asd";
+        assertEquals(expected, Strings.commonPrefix(s1, s2));
+        assertEquals(expected, Strings.commonSuffix(s3, s4));
+    }
+
+    /**
+     * 处理空串
+     */
+    @Test
+    public void empetyTest() {
         String empety = "";
         assertEquals(null, Strings.emptyToNull(empety));
         assertEquals(true, Strings.isNullOrEmpty(empety));
